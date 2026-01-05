ab0321752a66c4123056e452c2a63c189a0ba159
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-01-05 15:25:03 +0700

    first commit

---
+++ dist/app.js
@@ -0,0 +1,22 @@
+import cookieParser from "cookie-parser";
+import express from "express";
+import { userRouters } from "./modules/user/user.routes.js";
+import { errorMiddleware } from "./middlewares/error.middleware.js";
+import { authRoutes } from "./modules/auth/auth.routes.js";
+import { productRoutes } from "./modules/product/product.routes.js";
+export function createApp() {
+    const app = express();
+    app.use(express.json({ limit: "10mb" }));
+    app.use(cookieParser());
+    // check server run or not
+    app.get("/health", (_req, res) => res.json({ ok: true }));
+    // User module
+    app.use("/api/users", userRouters);
+    // Auth module
+    app.use("/api/auth", authRoutes);
+    // Product module
+    app.use("/api/products", productRoutes);
+    app.use(errorMiddleware);
+    return app;
+}
+//# sourceMappingURL=app.js.map
